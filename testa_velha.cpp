35024b49c6e1eb33b7dba03a8635c78b5f2f0a0c
Ahmed1878 <ahmedaza9z9@gmail.com>
2026-09-14 17:10:12 -0300

    Adiciona teste para vitoria do O

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -19,6 +19,12 @@ TEST_CASE( "Testa velha", "[single-file]" ) {
     REQUIRE( VerificaVelha(teste1) == 1 );
 
 
+}TEST_CASE( "Testa vitoria do O", "[single-file]" ) {
+    int teste2[3][3] = { { 2, 1, 0 },
+                         { 2, 1, 0 },
+                         { 2, 0, 1 } };
+
+    REQUIRE( VerificaVelha(teste2) == 2 );
 } 
  
 
