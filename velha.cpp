9898c23ce0ef35742f776f392f5bf084eb8e8f62
Ahmed1878 <ahmedaza9z9@gmail.com>
2026-09-15 17:42:18 -0300

    Implementa jogo impossivel

--- velha.cpp
+++ velha.cpp
@@ -8,10 +8,40 @@
  * \brief Verifica a situacao do jogo da velha.
  *
  * \param velha Tabuleiro 3x3.
- * \return 1 se X venceu, 2 se O venceu, 0 caso contrario.
+ * \return 1 se X venceu, 2 se O venceu, 0 se empate,
+ *         -1 se indefinido e -2 se impossivel.
  */
 int VerificaVelha(int velha[3][3])
 {
+    int quantidadeX = 0;
+    int quantidadeO = 0;
+    int vazias = 0;
+
+    // Conta X, O e casas vazias
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (velha[i][j] == 1) {
+                quantidadeX++;
+            } else if (velha[i][j] == 2) {
+                quantidadeO++;
+            } else {
+                vazias++;
+            }
+        }
+    }
+
+    // Situacoes impossiveis
+    if (quantidadeO > quantidadeX ||
+    quantidadeX > quantidadeO + 1) {
+    return -2;
+    }
+
+    // Jogo ainda indefinido
+    if (quantidadeX + quantidadeO == 0 ||
+        (quantidadeX == 1 && quantidadeO == 0)) {
+        return -1;
+    }
+
     // Verifica linhas
     for (int i = 0; i < 3; i++) {
         if (velha[i][0] == velha[i][1] &&
@@ -37,6 +67,7 @@ int VerificaVelha(int velha[3][3])
             }
         }
     }
+
     // Verifica diagonal principal
     if (velha[0][0] == velha[1][1] &&
         velha[1][1] == velha[2][2]) {
@@ -58,28 +89,11 @@ int VerificaVelha(int velha[3][3])
             return 2;
         }
     }
-    // Verifica se o jogo ainda esta indefinido
-    int quantidadeX = 0;
-    int quantidadeO = 0;
-    int vazias = 0;
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (velha[i][j] == 1) {
-                quantidadeX++;
-            } else if (velha[i][j] == 2) {
-                quantidadeO++;
-            } else if (velha[i][j] == 0) {
-                vazias++;
-            }
-        }
+    // Se nao houve vencedor e nao ha casas vazias, e empate
+    if (vazias == 0) {
+        return 0;
     }
 
-    if (quantidadeX + quantidadeO == 0) {
-        return -1;
-    }
-
-    if (quantidadeX == 1 && quantidadeO == 0) {
-        return -1;
-    }    return 0;
+    return 0;
 }
